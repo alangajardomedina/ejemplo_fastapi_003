93023d035fd14a658b68d8c92c21ba8832e1f05c
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2025-04-15 15:45:48 -0400

    modificacion - se agrega conexion oracle

--- app/main.py
+++ app/main.py
@@ -1,19 +1,52 @@
 #importamos la librería para crear la API
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import cx_Oracle
 
 #Vamos a crear una variable para la API:
 api = FastAPI()
 
-#Vamos a crear los endpoints de la API:
-@api.get("/")
-def root():
-    return {"mensaje": "Bienvenido desde API 2"}
+#VAMOS A REALIZAR UNA CONEXIÓN CON ORACLE:
+def get_conexion():
+    try:
+        dsn = cx_Oracle.makedsn(
+            "localhost",
+            1521,
+            service_name="orcl.duoc.com.cl"
+        )
+        conexion = cx_Oracle.connect(
+            user="ejemplo_fastapi",
+            password="ejemplo_fastapi",
+            dsn=dsn
+        )
+        return conexion
+    except Exception as e:
+        print("Error al conectar con oracle:",e)
+        raise
 
-@api.get("/{id}")
-def obtener_usuario(id: int):
-    usuarios = {
-        1: {"nombre": "Lalo", "apellido": "Cura"},
-        2: {"nombre": "Elsa", "apellido": "Pato"},
-        3: {"nombre": "Armando", "apellido": "Casas"}
-    }
-    return usuarios.get(id)
+#Método get que rescata los usuarios:
+@api.get("/usuarios")
+def get_usuarios():
+    try:
+        cone = get_conexion()
+        cursor = cone.cursor()
+        cursor.execute("SELECT rut, nombre, email FROM usuario")
+        rows = cursor.fetchall()
+        lista = []
+        for row in rows:
+            usu = {
+                "rut": row[0],
+                "nombre": row[1],
+                "email": row[2]
+            }
+            lista.append(usu)
+        return lista
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error al obtener usuarios: {str(e)}"
+        )
+    finally:
+        if 'cone' in locals():
+            cone.close()
+        if 'cursor' in locals():
+            cursor.close()
